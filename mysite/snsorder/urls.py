ca57787eb40f3a3f4bc309bb0f0887c5e2860e8d
ArturLee <liev.ar40@gmail.com>
2023-11-13 23:24:04 +0300

    button update item

--- mysite/snsorder/urls.py
+++ mysite/snsorder/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from snsorder.views import index, indexItem
+from snsorder.views import index, indexItem, add_item, update_item
+
+
+app_name = "snsorder"
 
 urlpatterns = [
     path('', index),
     path('<int:my_id>/', indexItem, name="detail"),
+    path('additem/', add_item, name="add_item"),
+    path('updateitem/<int:my_id>', update_item, name="update_item")
 
 ]
--- mysite/snsorder/views.py
+++ mysite/snsorder/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Product
 
@@ -19,4 +19,24 @@ def indexItem(request, my_id):
     return render(request, "snsorder/detail.html", context)
 
 
+def add_item(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        image = request.FILES["upload"]
+        item = Product(name=name, price=price, description=description, image=image)
+        item.save()
+    return render(request, "snsorder/additem.html" )
 
+def update_item(request, my_id):
+    item = Product.objects.get(id=my_id)
+    if request.method == "POST":
+        item.name = request.POST.get("name")
+        item.price = request.POST.get("price")
+        item.description = request.POST.get("description")
+        item.image = request.FILES.get("upload", item.image)
+        item.save()
+        return redirect("/snsorder/")
+    context = {"item": item}
+    return render(request, "snsorder/updateitem.html", context)
